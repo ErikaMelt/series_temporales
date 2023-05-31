5ee8216dc183a08eaae919a648e8d3d000f1bbc6
eomac <104458004+ErikaMelt@users.noreply.github.com>
2023-05-31 10:16:12 +0200

    update shiny serie temporal

--- series_temporales/shiny_practica_Equipo Textil/tabs/00_Modelos/tab_module.R
+++ series_temporales/shiny_practica_Equipo Textil/tabs/00_Modelos/tab_module.R
@@ -27,7 +27,7 @@ tab_0_ui <- function(id) {
         width = 12,
         h4("Gráfico de Serie Temporal",
            style = "color: #333; font-weight: bold; margin-bottom: 10px;"),
-        plotlyOutput(ns("grafico"))
+        plotlyOutput(ns("grafico_inicial"))
       )
     ),
     tags$hr(),
@@ -84,13 +84,14 @@ tab_0_ui <- function(id) {
       column(
         width = 6,
         box(
+          dateInput(ns("dateInput"), "Fecha de inicio:"),
           selectInput(
             ns("sel1"),
             choices = c("ARIMA", "ARIMA Multiplicativo"),
             label = "Elija su modelo"
           ),
           width = 12
-        )
+        ),
       )
     ),
     fluidRow(
@@ -108,9 +109,9 @@ tab_0_ui <- function(id) {
       column(
         width = 12,
         box(
-          title = "Resultado de la predicción ARIMA",
+          title = "Coeficientes del modelo ARIMA",
           width = 12,
-          verbatimTextOutput(ns("forecast_result"))
+          verbatimTextOutput(ns("arima_coefs"))
         )
       )
     ),
@@ -118,20 +119,26 @@ tab_0_ui <- function(id) {
       column(
         width = 12,
         box(
-          title = "MAE - MSE y BIC",
+          title = "Predicciones",
           width = 12,
-          verbatimTextOutput(ns("errorMetricsOutput")),
-          verbatimTextOutput(ns("bicOutput"))
-          
+          verbatimTextOutput(ns("arima_predictions"))
         )
       )
-    )
+    ),
+    fluidRow(
+      column(
+        width = 12,
+        box(
+          title = "BIC",
+          width = 12,
+          verbatimTextOutput(ns("bicOutput"))
+        )
+      )
+    ),
   )
 }
 
 
-
-# Server function
 # Server function
 tab_0_server <- function(input, output, session) {
   
@@ -169,14 +176,33 @@ tab_0_server <- function(input, output, session) {
               class = 'cell-border stripe')
   })
   
-  output$grafico <- renderPlotly({
+  output$grafico_inicial <- renderPlotly({
     # Crear el gráfico de serie temporal utilizando ggplot2 y convertirlo a plotly
     p <- ggplot(dataset(), aes(x = Fecha, y = Serie)) +
       geom_line() +
-      labs(x = "Fecha", y = "Serie") +
-      theme_bw()
+      labs(y = "Valor de la Serie") 
     
-    ggplotly(p)
+    p <- p + scale_x_date(
+      date_breaks = "8 months",  # Intervalo de 6 meses entre las etiquetas de fecha
+      date_labels = "%b %Y",  # Formato de las etiquetas de fecha (por ejemplo, "Jun 2023")
+      expand = c(0.01, 0)  # Ajuste los márgenes del eje x para evitar solapamientos
+    )
+    
+    
+    # Ejemplo: Agregar una línea de referencia en el valor medio de la serie
+    mean_value <- mean(dataset()$Serie)
+    p <- p + geom_hline(yintercept = mean_value, linetype = "dashed", color = "red")
+    
+    p <- p + theme_minimal()
+    p <- p + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+    
+    # Convertir a plotly y agregar características interactivas
+    p <- ggplotly(p) %>% 
+      layout(
+        hovermode = "x",  # Información emergente al pasar el cursor
+        dragmode = "zoom",  # Habilitar la función de zoom
+        selectdirection = "h" 
+      )
   })
   
   predict_arima <- eventReactive(input$predictButton, {
@@ -186,9 +212,19 @@ tab_0_server <- function(input, output, session) {
       stop("Valores de entrada no válidos. Por favor, ingrese parámetros válidos.")
     }
     
+    if (is.null(input$dateInput)) {
+      stop("Por favor, ingrese una fecha válida.")
+    }
+    
+    # Convertir la fecha ingresada por el usuario en un objeto de fecha en R
+    input_date <- as.Date(input$dateInput)
+    
     if (input$sel1 == "ARIMA") {
-      # Ajustar el modelo ARIMA utilizando el conjunto de datos completo
-      arima_model <- arima(dataset()$Serie, order = c(input$p, input$d, input$q))
+      # Filtrar el conjunto de datos hasta la fecha ingresada por el usuario
+      filtered_data <- dataset()[dataset()$Fecha <= input_date, ]
+      
+      # Ajustar el modelo ARIMA utilizando el conjunto de datos filtrado
+      arima_model <- arima(filtered_data$Serie, order = c(input$p, input$d, input$q))
       
       # Obtener las predicciones para el horizonte especificado
       forecast_result <- predict(arima_model, n.ahead = input$horizon)
@@ -200,24 +236,29 @@ tab_0_server <- function(input, output, session) {
       num_params <- length(arima_model$coef)
       
       # Calcular el BIC manualmente
-      n <- length(dataset()$Serie)
+      n <- length(filtered_data$Serie)
       bic <- -2 * logLik(arima_model) + num_params * log(n)
       
       # Mostrar el valor del BIC
       cat("BIC:", bic, "\n")
       
       # Calcular los errores utilizando los valores reales y las predicciones
-      actual_values <- tail(dataset()$Serie, input$horizon)
+      actual_values <- tail(filtered_data$Serie, input$horizon)
       errors <- actual_values - forecast_result$pred
       mae <- mean(abs(errors))
       mse <- mean(errors^2)
       
       # Agregar las métricas de error al resultado de la predicción
+      forecast_result$BIC <- bic
       forecast_result$MAE <- mae
       forecast_result$MSE <- mse
+      
     } else if (input$sel1 == "ARIMA Multiplicativo") {
-      # Ajustar el modelo ARIMA multiplicativo utilizando el conjunto de datos completo
-      arima_model <- auto.arima(dataset()$Serie, seasonal = TRUE, lambda = "auto")
+      # Filtrar el conjunto de datos hasta la fecha ingresada por el usuario
+      filtered_data <- dataset()[dataset()$Fecha <= input_date, ]
+      
+      # Ajustar el modelo ARIMA multiplicativo utilizando el conjunto de datos filtrado
+      arima_model <- auto.arima(filtered_data$Serie, seasonal = TRUE, lambda = "auto")
       
       # Obtener las predicciones para el horizonte especificado
       forecast_result <- forecast(arima_model, h = input$horizon)
@@ -229,19 +270,20 @@ tab_0_server <- function(input, output, session) {
       num_params <- length(arima_model$coef)
       
       # Calcular el BIC manualmente
-      n <- length(dataset()$Serie)
+      n <- length(filtered_data$Serie)
       bic <- -2 * logLik(arima_model) + num_params * log(n)
       
       # Mostrar el valor del BIC
       cat("BIC:", bic, "\n")
       
       # Calcular los errores utilizando los valores reales y las predicciones
-      actual_values <- tail(dataset()$Serie, input$horizon)
+      actual_values <- tail(filtered_data$Serie, input$horizon)
       errors <- actual_values - forecast_result$mean
       mae <- mean(abs(errors))
       mse <- mean(errors^2)
       
       # Agregar las métricas de error al resultado de la predicción
+      forecast_result$BIC <- bic
       forecast_result$MAE <- mae
       forecast_result$MSE <- mse
     }
@@ -249,35 +291,33 @@ tab_0_server <- function(input, output, session) {
     return(list(arima_model = arima_model, forecast_result = forecast_result))
   })
   
-  output$forecast_result <- renderPrint({
+  
+  output$plot <- renderPlotly({
+    # Obtener la fecha de inicio ingresada por el usuario
+    fecha_inicio <- input$fecha_inicio
+    
+    # Obtener los resultados de la predicción ARIMA
+    predict_arima_result <- predict_arima()
+    
+    # Generar el gráfico de predicción de serie temporal
+    plt_prediction_serie(dataset, predict_arima_result$pred, predict_arima_result, fecha_inicio)
+  })
+  
+  
+  output$arima_coefs <- renderPrint({
+    predict_arima_result <- predict_arima()
+    predict_arima_result$arima_model$coef
+  })
+  
+  output$arima_predictions <- renderPrint({
     predict_arima_result <- predict_arima()
     predict_arima_result$forecast_result
   })
   
-  output$errorMetricsOutput <- renderText({
-    predict_arima_result <- predict_arima()
-    forecast_result <- predict_arima_result$forecast_result
-    
-    if (is.null(forecast_result$MAE) || is.null(forecast_result$MSE)) {
-      return(NULL)
-    }
-    
-    mae <- forecast_result$MAE
-    mse <- forecast_result$MSE
-    
-    paste("MAE:", sprintf("%.2f", mae), "\n", "MSE:", sprintf("%.2f", mse))
-  })
-  
   output$bicOutput <- renderPrint({
     predict_arima_result <- predict_arima()
-    arima_model <- predict_arima_result$arima_model
-    
-    # Calcular el BIC manualmente
-    n <- length(dataset()$Serie)
-    bic <- -2 * logLik(arima_model) + length(arima_model$coef) * log(n)
-    
-    # Mostrar el valor del BIC
-    cat("BIC:", bic, "\n")
+    predict_arima_result$forecast_result$BIC
   })
+  
 }
 
